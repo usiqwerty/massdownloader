10b3c0f8a78fc74a2891bc533e52aac762890f98
usiqwerty <37992525+usiqwerty@users.noreply.github.com>
2020-10-24 15:01:08 +0500

    Create md.sh

---
+++ md.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#http://fantserials.org/doctor-who-classic.php
+echo -e "\033[0;42mWelcome to \033[0;43mmass downloader\033[0;42m by usiqwerty\033[0m"
+printf "Download directory: " && read D
+printf "Linklist: " && read S
+[[ $S == $D ]] && echo "See usage, exiting" &&  exit
+cd $D
+test -d $D/$S || mkdir $D/$S --parent
+cd $D/$S
+echo "Downloading to: " $D/$S/
+sleep 4
+a=$(cat ../$S)
+for i in $a
+do
+  	wget $i
+done
